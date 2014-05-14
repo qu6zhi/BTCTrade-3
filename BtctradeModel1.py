84d0a01ab905bc4081ff2f8f6fd4be5e3dfa2006
richard119 <richard119@gmail.com>
2014-05-14 23:37:58 +0800

    Add New Model

---
+++ BtctradeModel1.py
@@ -0,0 +1,219 @@
+import okcoin
+from bitf import *
+import json
+from  datetime  import  *
+import time
+from huobi import *
+from btceapi import *
+from btcapiall import *
+
+_usd2cny = 6.2595
+
+def compare(ok,bf):
+   diff = bf['btc']*bf['price'] - bf['usd']
+   if diff < -20:
+      bf_status = 'cash'
+   elif diff > 20:
+      bf_status = 'btc'
+   else:
+      bf_status = 'balance'
+      print "BF btc is %f,BF usd is %f,BF account status is %s, diff is %f."%(bf['btc'],bf['usd'],bf_status,diff)
+
+   ok_price = ok['price']
+   bf_price = bf['price']*_usd2cny
+   dif_price = bf_price - ok_price
+   rate = dif_price / ok_price
+   print "OK price is %f, BF price is %f, Price different is %f, rate is %f" %(ok_price,bf_price,dif_price,rate)
+   
+   if rate > 0.016:
+      if bf_status == 'cash':
+	 print "Reach BF sell point, BF sold. Hold"
+	 return "Hold"
+      else:
+	 print "Reach BF sell point. Plan to sell"
+	 return "SellBF"
+   elif rate < 0.0015 and rate > -0.0015:
+      if bf_status == 'balance':
+	 print "Reach BF Balance Point, BF is balance"
+	 return "Hold"
+      else:
+	 print "Reach BF balance Point. Plan to balance"
+	 return "BalanceBF"
+   elif rate < -0.016:
+      if bf_status == 'btc':
+	 print "Reach BF Buy Point, BF have bought"
+	 return "Hold"
+      else:
+	 print "Reach BF Buy Point, Plan to buy"
+	 return "BuyBF"
+   else:
+      return "Hold as Normal"
+
+def bf_buy_okhb(bfx,info_bf,auth_ok,info_ok,hbx,info_hb,buy_btc):
+
+   if info_hb['btc'] + info_ok['btc'] < buy_btc:
+      print "Warning! Not enough btc %f to sell,hb btc is %f, ok btc is %f"%(buy_btc,info_hb['btc'],info_ok['btc'])
+      return "Fail"
+
+   dep_btc = info_hb['sellnum'] / 3 + info_ok['sellnum'] /3
+   if dep_btc < buy_btc:
+      print "Warning! Not enough depth btc %f to sell, hb depth btc is %f, ok depth btc is %f"%(buy_btc,info_hb['sellnum'],info_ok['sellnum'])
+      return "Fail"
+   
+   flag = ""
+   # Case 0: sell all btc in OK
+   if buy_btc > info_ok['sellnum'] / 3 and buy_btc < info_ok['btc']:
+      result = ok_sell(auth_ok,info_ok,buy_btc)
+      print result
+      flag =  "OK"
+   
+   # Case 1: Sell all btc in OK and sell rest of it in HB
+   diff_ok1 = buy_btc - info_ok['btc']
+   trad_ok1 = info_ok['sellnum'] / 3  - info_ok['btc']
+   diff_hb1 = info_hb['btc'] - diff_ok1
+   trad_hb1 = info_hb['sellnum'] / 3 - diff_ok1
+   if flag == "" and diff_ok1 > 0 and trad_ok1 > 0 and diff_hb1 >0 and trad_hb1 > 0:
+      result = ok_sell(auth_ok,info_ok,info_ok['btc'])
+      print result
+      result = hb_sell(hbx,info_hb,diff_ok1)
+      print result
+      flag = "OK"
+      
+   # Case 2: Sell all btc in HB and sell rest of it in OK
+   diff_hb2 = buy_btc - info_hb['btc']
+   trad_hb2 = info_hb['sellnum'] / 3  - info_hb['btc']
+   diff_ok2 = info_ok['btc'] - diff_hb2
+   trad_ok2 = info_ok['sellnum'] / 3 - diff_hb2
+   if flag == "" and diff_hb2 > 0 and trad_hb2 > 0 and diff_ok2 >0 and trad_ok2 > 0:
+      result = hb_sell(hbx,info_hb,info_hb['btc'])
+      print result
+      result = ok_sell(auth_ok,info_ok,diff_hb2)
+      print result
+      flag = "OK"
+
+   if flag == "OK":
+# BF buy
+      result = bf_buy(bfx,info_bf,buy_btc)
+      print result
+      return "OK"
+   else:
+      print "Can not find trade chance, OK info is"
+      print info_ok
+      print "HB info is"
+      print info_ok
+      return "Fail"
+
+def bf_sell_okhb(bfx,info_bf,auth_ok,info_ok,hbx,info_hb,sell_btc):
+   if info_hb['buy'] == 0 or info_ok['buy'] == 0:
+      print "Warning! Not enough depth btc to buy btc %f,hb buy is %f, ok buy is %f"%(sell_btc,info_hb['buy'],info_ok['buy'])
+      return "Fail"
+
+   if info_hb['cny'] / info_hb['buy']  + info_ok['cny'] / info_ok['buy'] < sell_btc:
+      print "Warning! Not enough cash to buy btc %f,hb cny is %f, ok cny is %f"%(sell_btc,info_hb['cny'],info_ok['cny'])
+      return "Fail"
+
+   dep_btc = info_hb['buynum'] / 3 + info_ok['buynum'] /3
+   if dep_btc < sell_btc:
+      print "Warning! Not enough depth btc %f to buy, hb depth btc is %f, ok depth btc is %f"%(sell_btc,info_hb['buynum'],info_ok['buynum'])
+      return "Fail"
+   
+   flag = ""
+   # Case 0: buy all btc in OK
+   if sell_btc > info_ok['buynum'] / 3 and sell_btc < info_ok['cny'] / info_ok['buy']:
+      result = ok_buy(auth_ok,info_ok,sell_btc)
+      print result
+      flag =  "OK"
+   
+   # Case 1: Buy all btc in OK and buy rest of it in HB
+   btc_ok1 = info_ok['cny'] / info_ok['buy'] - 0.0003
+   diff_ok1 = sell_btc - btc_ok1
+   trad_ok1 = info_ok['buynum'] / 3  - btc_ok1
+   diff_hb1 = info_hb['cny'] / info_hb['buy'] - diff_ok1
+   trad_hb1 = info_hb['buynum'] / 3 - diff_ok1
+   if flag == "" and diff_ok1 > 0 and trad_ok1 > 0 and diff_hb1 >0 and trad_hb1 > 0:
+      result = ok_buy(auth_ok,info_ok,btc_ok1)
+      print result
+      result = hb_buy(hbx,info_hb,diff_ok1)
+      print result
+      flag = "OK"
+      
+   # Case 2: Sell all btc in HB and sell rest of it in OK
+   btc_hb2 = info_hb['cny'] / info_hb['buy'] - 0.0003
+   diff_hb2 = sell_btc - btc_hb2
+   trad_hb2 = info_hb['buynum'] / 3  - btc_hb2
+   diff_ok2 = info_ok['cny'] / info_ok['buy'] - diff_hb2
+   trad_ok2 = info_ok['buynum'] / 3 - diff_hb2
+   if flag == "" and diff_hb2 > 0 and trad_hb2 > 0 and diff_ok2 >0 and trad_ok2 > 0:
+      result = hb_buy(hbx,info_hb,btc_hb2)
+      print result
+      result = ok_buy(auth_ok,info_ok,diff_hb2)
+      print result
+      flag = "OK"
+
+   if flag == "OK":
+# BF buy
+      result = bf_sell(bfx,info_bf,sell_btc)
+      print result
+      return "OK"
+   else:
+      print "Can not find trade chance, OK info is"
+      print info_ok
+      print "HB info is"
+      print info_ok
+
+
+
+if __name__ == "__main__":
+   auth_ok = okcoin.TradeAPI('','')
+   bfx = Bitfinex()
+   bfx.secret = ''
+   bfx.key = ''
+   hbx = HuoBi('','')
+   bex = btce('','')
+
+   sleep_time = 30
+   error = 0
+   trade_num = 0
+   while 1:
+      print   'time:' , datetime.now(), 'Trade num:', trade_num
+
+      try:
+	 info_ok = get_info_ok(auth_ok)
+	 info_bf = get_info_bf(bfx)
+	 info_hb = get_info_hb(hbx)
+      except Exception,e:
+         print str(e)
+	 error = 1
+      
+      if error == 1:
+	 error = 0
+	 time.sleep(sleep_time)
+	 continue
+      
+      res = compare(info_ok,info_bf)
+      print res
+      trade = ""
+      if res == "BuyBF":
+	 buy_btc = info_bf['usd']/info_bf['buy'] - 0.0003
+	 trade = bf_buy_okhb(bfx,info_bf,auth_ok,info_ok,hbx,info_hb,buy_btc)
+	 print trade
+      elif res == "SellBF":
+	 sell_btc = info_bf['btc']
+	 trade = bf_sell_okhb(bfx,info_bf,auth_ok,info_ok,hbx,info_hb,sell_btc)
+	 print trade
+      elif res == "BalanceBF":
+	 diff_usd = info_bf['btc'] * info_bf['sell'] - info_bf['usd']
+	 if diff_usd > 20:
+	    sell_btc = diff_usd / 2 / info_bf['sell']
+	    trade = bf_sell_okhb(bfx,info_bf,auth_ok,info_ok,hbx,info_hb,sell_btc)
+	    print trade
+	 elif diff_usd < -20:
+	    buy_btc = (-1) * diff_usd / 2 / info_bf['buy']
+	    trade = bf_buy_okhb(bfx,info_bf,auth_ok,info_ok,hbx,info_hb,buy_btc)
+	    print trade
+      
+      if trade == "OK":
+	 trade_num += 1
+
+      time.sleep(sleep_time)
+
---
+++ btcapiall.py
@@ -0,0 +1,267 @@
+import okcoin
+from bitf import *
+import json
+from  datetime  import  *
+import time
+from huobi import *
+from btceapi import *
+
+def get_info_be(btx):
+	tick = btx.get_tick()
+	info_be = {}
+	info_be['price'] = float(tick['ticker']['last'])
+
+	depth_be = btx.get_depth()
+
+	up_amt = info_be['price'] * 1.003
+	down_amt = info_be['price'] * 0.997
+	
+	info_be['buy'] = 0 
+	info_be['buynum'] = 0
+	for i in range(0,149):
+		ask_amt = float(depth_be['asks'][i][0])
+		if ask_amt <= up_amt:
+			info_be['buy'] = ask_amt
+			info_be['buynum'] += float(depth_be['asks'][i][1])
+		else:
+			break
+	
+	info_be['sell'] = 0 
+	info_be['sellnum'] = 0
+	for i in range(0,149):
+		bid_amt = float(depth_be['bids'][i][0])
+		if bid_amt >= down_amt:
+			info_be['sell'] = bid_amt
+			info_be['sellnum'] += float(depth_be['bids'][i][1])
+		else:
+			break
+
+	acc = bex.getInfo()
+	info_be['usd'] = float(acc['return']['funds']['usd'])
+	info_be['btc'] = float(acc['return']['funds']['btc'])
+# be have not frazen usd amount , return open order 
+	info_be['f_usd'] = acc['return']['open_orders']
+	info_be['f_btc'] = acc['return']['open_orders']
+	
+	return info_be
+
+def be_sell(bex,be,btc):
+	price = be['sell']
+	total = price * btc
+	strP =  "We are going to sell %f BTC for %f in BE, total is %f" % (btc, price,total)
+	print strP
+	writefile(strP)
+
+	result = bex.Trade('btc_usd','sell',price,btc)
+	print result
+
+def be_buy(bex,be,btc):
+	price = be['buy']
+	total = price * btc
+	strP =  "We are going to buy %f BTC for %f in BE, total is %f" % (btc, price,total)
+	print strP
+	writefile(strP)
+
+	result = bex.Trade('btc_usd','buy',price,btc)
+	print result
+
+def get_info_hb(hbx):
+	tick = hbx.get_tick()
+	info_hb = {}
+	info_hb['price'] = float(tick['ticker']['last'])  
+
+	depth_hb = hbx.get_depth()
+	
+	up_amt = info_hb['price'] * 1.003
+	down_amt = info_hb['price'] * 0.997
+
+	info_hb['buy'] = 0
+	info_hb['buynum'] = 0
+	for i in range(299,1,-1):
+		ask_amt = float(depth_hb['asks'][i][0])
+		if ask_amt <= up_amt:
+			info_hb['buy'] = ask_amt 
+        		info_hb['buynum'] += float(depth_hb['asks'][i][1])
+		else:
+			break
+
+	info_hb['sell'] = 0
+	info_hb['sellnum'] = 0
+	for i in range(0,299):
+		bid_amt = float(depth_hb['bids'][i][0])
+		if bid_amt >= down_amt:
+			info_hb['sell'] = bid_amt 
+        		info_hb['sellnum'] += float(depth_hb['bids'][i][1])
+		else:
+			break
+
+	acc = hbx.get_account_info()
+	info_hb['cny'] = float(acc['available_cny_display'])
+	info_hb['btc'] = float(acc['available_btc_display'])
+	info_hb['f_cny'] = float(acc['frozen_cny_display'])
+	info_hb['f_btc'] = float(acc['frozen_btc_display'])
+
+	return info_hb
+
+def hb_sell(hbx,hb,btc):
+	price = hb['sell']
+	total = price * btc
+	strP =  "We are going to buy %f BTC for %f in HB, total is %f" % (btc, price,total)
+	print strP
+	writefile(strP)
+
+	result = hbx.sell(price,btc)
+	print result
+
+def hb_buy(hbx,hb,btc):
+	price = hb['buy']
+	total = price * btc
+	strP =  "We are going to buy %f BTC for %f in HB,total is %f" % (btc, price,total)
+	print strP
+	writefile(strP)
+
+	result = hbx.buy(price,btc)
+	print result
+
+def get_info_ok(authobj):
+	m_okcoin = okcoin.MarketData()
+	tick_ok = m_okcoin.ticker('btc_cny')
+	info_ok = {}
+	info_ok['price'] = float(tick_ok.last)
+
+	deph_ok = m_okcoin.get_depth('btc_cny')
+
+	up_amt = info_ok['price'] * 1.003
+	down_amt = info_ok['price'] * 0.997
+
+	info_ok['buy'] = 0
+	info_ok['buynum'] = 0
+	for i in range(59,1,-1):
+		ask_amt = float(deph_ok.asks[i][0])
+		if ask_amt <= up_amt:
+			info_ok['buy'] = ask_amt 
+        		info_ok['buynum'] += float(deph_ok.asks[i][1])
+		else:
+			break
+ 
+        info_ok['sell'] = 0
+	info_ok['sellnum'] = 0
+	for i in range(0,59):
+		bid_amt = float(deph_ok.bids[i][0])
+		if bid_amt >= down_amt:
+			info_ok['sell'] = bid_amt 
+        		info_ok['sellnum'] += float(deph_ok.bids[i][1])
+		else:
+			break 
+
+	acc =  authobj.get_info()	
+	info_ok['cny'] = float(acc['info']['funds']['free']['cny'])
+	info_ok['btc'] = float(acc['info']['funds']['free']['btc'])
+	info_ok['f_cny'] = float(acc['info']['funds']['freezed']['cny'])
+	info_ok['f_btc'] = float(acc['info']['funds']['freezed']['btc'])
+
+	return info_ok
+
+def ok_sell(auth,ok,btc):
+	price = ok['sell']
+	total = price * btc
+	strP =  "We are going to sell %f BTC for %f in OK, total is %f" % (btc, price,total)
+	print strP
+	writefile(strP)
+
+	result = auth.trade('btc_cny','sell',price,btc)
+	print result
+	
+def ok_buy(auth,ok,btc):
+	price = ok['buy']
+	total = price * btc
+	strP =  "We are going to buy %f BTC for %f in OK, total is %f" % (btc, price,total)
+	print strP
+	writefile(strP)
+
+	result = auth.trade('btc_cny','buy',price,btc)
+	print result
+
+def get_info_bf(bfx):
+	tick_bf = bfx.ticker()
+	info_bf = {}
+	info_bf['price'] = float(tick_bf['last_price'])
+
+	preload = {}
+	preload['symbol'] = 'btcusd'
+	deph_bf = bfx.book(preload)
+
+	up_amt = info_bf['price'] * 1.003
+	down_amt = info_bf['price'] * 0.997
+        
+	info_bf['buy'] = 0
+	info_bf['buynum'] = 0
+	for i in range(0,59):
+		ask_amt = float(deph_bf['asks'][i]['price'])
+		if ask_amt <= up_amt:
+			info_bf['buy'] = ask_amt 
+        		info_bf['buynum'] += float(deph_bf['asks'][i]['amount'])
+		else:
+			break 
+        
+	info_bf['sell'] = 0
+	info_bf['sellnum'] = 0
+	for i in range(0,59):
+		bid_amt = float(deph_bf['bids'][i]['price']) 
+		if bid_amt >= down_amt:
+			info_bf['sell'] = bid_amt 
+        		info_bf['sellnum'] +=float(deph_bf['bids'][i]['amount'])
+		else:
+			break 
+	
+	acc = bfx.balances()
+	for item in acc:
+		if item['currency'] == 'btc' and item['type'] == 'exchange':
+			info_bf['btc'] = float(item['available'])
+			info_bf['f_btc'] = float(item['amount']) - info_bf['btc']
+		if item['currency'] == 'usd' and item['type'] == 'exchange':
+			info_bf['usd'] = float(item['available'])
+			info_bf['f_usd'] = float(item['amount']) - info_bf['usd']
+	
+	return info_bf
+
+def bf_buy(bfx,bf,btc):
+	price = bf['buy']
+	total = price * btc
+	strP =  "We are going to buy %f BTC for %f in BF, total is %f" % (btc, price, total)
+	print strP
+	writefile(strP)
+
+	payload = {}
+	payload['symbol'] = 'btcusd'
+	payload['amount'] = str(btc)
+	payload['price'] = str(price)
+	payload['exchange'] = 'all'
+	payload['side'] = 'buy'
+	payload['type'] = 'exchange limit'
+	border = bfx.order_new(payload)
+	print border	
+
+def bf_sell(bfx,bf,btc):
+	price = bf['sell']
+	total = price * btc
+	strP = "We are going to sell %f BTC for %f in BF, total is %f" % (btc, price, total)
+	print strP
+	writefile(strP)
+
+	payload = {}
+	payload['symbol'] = 'btcusd'
+	payload['amount'] = str(btc)
+	payload['price'] = str(price)
+	payload['exchange'] = 'all'
+	payload['side'] = 'sell'
+	payload['type'] = 'exchange limit'
+	border = bfx.order_new(payload)
+	print border	
+
+def writefile(str):
+	str = datetime.now().strftime("%Y-%m-%d %H:%M:%S ") + str
+   	file_object = open('theLog1.txt', 'a')
+	file_object.write(str)
+	file_object.write('\n')
+	file_object.close( )	
